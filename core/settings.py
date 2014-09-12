570a7f3d3a532527085958aac03c32c776b05ad5
Sergey Vorontsov <cepera.voron@gmail.com>
2014-09-12 14:20:08 +0300

    basic installation

--- core/settings.py
+++ core/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smyt_test',
+        'USER': 'smyt',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
